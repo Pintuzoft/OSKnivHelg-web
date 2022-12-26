79c9fe1060aedac868d288367c19191b5d789198
DreamHealer <swehtc@gmail.com>
2022-12-26 21:28:28 +0000

    .

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ echo "    </div>\n";
 
 
 /* CONTENT */
-echo "    <div id='content' class='container-fluid'>\n";
+echo "    <div id='content' class='container-md'>\n";
 echo "      <table id='eventlist' class='table table-striped table-bordered' style='width:100%'>\n";
 
 echo "        <thead>\n";
