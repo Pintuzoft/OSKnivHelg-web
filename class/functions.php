37f3e342756c11cce52ff87717bd1f8ecafb273d
DreamHealer <swehtc@gmail.com>
2022-12-26 08:49:05 +0000

    .

--- class/functions.php
+++ class/functions.php
@@ -3,13 +3,14 @@ include_once 'mysql.php';
 
 function getEventList ( ) {
     global $mysql;
-    $eList = new ArrayList();
+    $eList = new ArrayList ( );
     $query = "SELECT * FROM event ORDER BY time DESC";
-    $stmt = $mysql->prepare($query);
-    $stmt->execute();
-    $stmt->bind_result($time, $attacker, $attackerid, $victim, $victimid, $points);
-    while ( $stmt->fetch()) {
-        $eList->add(new Event($time,$attacker,$attackerid,$victim,$victimid,points));
+    $stmt = $mysql->prepare ( $query );
+    $stmt->execute ( );
+    $stmt->store_result ( );
+    $stmt->bind_result ( $time, $attacker, $attackerid, $victim, $victimid, $points );
+    while ( $stmt->fetch ( ) ) {
+        $eList->add ( new Event ( $time, $attacker, $attackerid, $victim, $victimid, $points ) );
     }
     return $eList;
 }
