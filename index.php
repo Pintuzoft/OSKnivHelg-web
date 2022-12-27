e9c7ab912d84e1c89954580d6a3715c0c548d8a5
DreamHealer <swehtc@gmail.com>
2022-12-27 05:59:09 +0000

    ...

--- index.php
+++ index.php
@@ -40,31 +40,9 @@ echo "    </div>\n";
 
 /* CONTENT LEFT*/
 echo "    <div id='content' class='container-fluid'>\n";
-echo "       <div id='content-main' class='container-md row'>\n";
+echo "       <div id='content-main' class='container row'>\n";
 echo "         <div id='content-left' class='col'>\n";
-echo "          <table id='eventlist' class='table table-striped table-bordered'>\n";
 
-echo "              <thead>\n";
-echo "                  <tr>\n";
-echo "                      <th>Time</th>\n";
-echo "                      <th>Attacker</th>\n";
-echo "                      <th>Victim</th>\n";
-echo "                  </tr>\n";
-echo "              </thead>\n";
-
-echo "              <tbody>\n";
-
-$eList = getEventList();
-foreach ( $eList->getArray() as $event ) {
-    echo "                  <tr>\n";
-    echo "                      <td>".$event->getTime()."</td>\n";
-    echo "                      <td>".$event->getAttacker()." [".$event->getPoints()."p]</td>\n";
-    echo "                      <td>".$event->getVictim()." [-".$event->getPoints()."p]</td>\n";
-    echo "                  </tr>\n";
-}
-
-echo "              </tbody>\n";
-echo "          </table>\n";
 echo "        </div>\n";
 
 echo "        <div id='content-right' class='col'>\n";
