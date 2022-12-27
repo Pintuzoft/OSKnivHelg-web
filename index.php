ab5988c51fc41a6a99fc8f3bfc4643177b3ab20f
DreamHealer <swehtc@gmail.com>
2022-12-27 06:06:08 +0000

    ...

--- index.php
+++ index.php
@@ -43,12 +43,12 @@ echo "    <div id='content' class='container-fluid'>\n";
 
 echo "      <div id='content-main' class='container'>\n";
 echo "        <div id='content-main-inner' class='row'>\n";
-echo "          <div id='content-left' class='col-md-3'>\n";
+echo "          <div id='content-left' class='col-md-3 blue'>\n";
 echo "            <p>left!</p>\n";
 echo "          </div>\n";
 
 /* CONTENT RIGHT */
-echo "          <div id='content-right' class='col-md-9'>\n";
+echo "          <div id='content-right' class='col-md-9 brown'>\n";
 
 echo "            <p>right!</p>\n";
 
