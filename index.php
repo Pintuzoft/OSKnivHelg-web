adb94c408c0ef50bf3a1f923825650d59cc2762a
DreamHealer <swehtc@gmail.com>
2023-01-05 09:38:02 +0000

    ...

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include_once 'class/user.php';
 echo "<html>\n";
 
 echo "  <head>\n";
-echo "    <title>My Title</title>\n";
+echo "    <title>Knivhelg - Kniva en Admin!</title>\n";
 echo "    <link rel='stylesheet' type='text/css' href='css/default.css'>\n";
 
 /* JQUERY */
